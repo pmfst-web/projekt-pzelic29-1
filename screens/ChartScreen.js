bcc9538983c5a101de86f7d570646d0fba5d9498
pzelic29 <pzelic29@gmail.com>
2023-05-09 16:38:47 +0200

    making some minor changes in chartscreen so it can display total amount by category or total percentage by category

--- screens/ChartScreen.js
+++ screens/ChartScreen.js
@@ -1,25 +1,48 @@
-import React, { useContext } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useContext } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { PieChart } from "react-native-chart-kit";
 import { ExpensesContext } from "../store/store";
 
-const ChartScreen = () => {
+function ChartScreen() {
   const { expenses } = useContext(ExpensesContext);
+  const [displayPercentage, setDisplayPercentage] = useState(false);
 
   const expensesByCategory = expenses.reduce((acc, expense) => {
     const { category } = expense;
-    acc[category] = acc[category] ? acc[category] + expense.amount : expense.amount;
+    acc[category] = acc[category]
+      ? acc[category] + expense.amount
+      : expense.amount;
     return acc;
   }, {});
 
-  const data = Object.keys(expensesByCategory).map((category) => ({
-    name: category,
-    value: expensesByCategory[category],
-    color: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
-  }));
+  const totalExpense = Object.values(expensesByCategory).reduce(
+    (total, expense) => total + expense,
+    0
+  );
+
+  const data = Object.keys(expensesByCategory).map((category) => {
+    const value = expensesByCategory[category];
+    const label = displayPercentage
+      ? `${((value / totalExpense) * 100).toFixed(2)}%`
+      : `${value.toFixed(2)}€`;
+    return {
+      name: category,
+      value,
+      label,
+      color: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+    };
+  });
 
   return (
     <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setDisplayPercentage(!displayPercentage)}
+      >
+        <Text style={styles.buttonText}>
+          {displayPercentage ? "Amount" : "Percentage"}
+        </Text>
+      </TouchableOpacity>
       <PieChart
         data={data}
         width={300}
@@ -40,44 +63,62 @@ const ChartScreen = () => {
         <Text style={styles.legendTitle}>Legend</Text>
         {data.map((item, index) => (
           <View style={styles.legendItem} key={index}>
-            <View style={[styles.legendColor, { backgroundColor: item.color }]} />
-            <Text style={styles.legendText}>{item.name} - {item.value.toFixed(2)}€</Text>
+            <View
+              style={[styles.legendColor, { backgroundColor: item.color }]}
+            />
+            <Text style={styles.legendText}>
+              {item.name} - {item.label}
+            </Text>
           </View>
         ))}
       </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: "#80ced6",
   },
-  legendContainer: {
-    flexDirection: "column",
-    marginTop: 30,
-    backgroundColor: "white",
+  button: {
+    backgroundColor: '#0066CC',
     padding: 10,
     borderRadius: 5,
-    borderWidth: 1,
-    borderColor: "#555",
+    marginVertical: 20,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  legendContainer: {
+    marginTop: 20,
+    paddingHorizontal: 20,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 5,
+    shadowColor: '#000000',
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    shadowOffset: { width: 0, height: 2 },
   },
   legendTitle: {
     fontSize: 18,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginBottom: 10,
+    textAlign: 'center',
   },
   legendItem: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     marginVertical: 5,
   },
   legendColor: {
     width: 20,
     height: 20,
+    borderRadius: 10,
     marginRight: 10,
   },
   legendText: {
